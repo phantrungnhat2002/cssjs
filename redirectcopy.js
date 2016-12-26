4bf7dcebe13e67db04eddb8983a53b8fb9767920
phantrungnhat2002 <phantrungnhat2002@gmail.com>
2016-12-26 19:43:37 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Ngưng Copy Ngay!!!: <a href='http://quanaotapgymhcm.blogspot.com'>http://faceseo.vn"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Ngưng Copy Ngay!!!: <a href='http://quanaotapgymhcm.blogspot.com'>http://duan-hadocentrosa.com"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
